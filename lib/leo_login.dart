87b6e567e834f00d3f2944e4af392ed8ce6ae8a5
LENOVO <leonelsyelo1@gmail.com>
2026-05-06 11:36:11 +0700

    kembut kembut

---
+++ lib/leo_login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LeoLogin extends StatefulWidget {
+  const LeoLogin({super.key});
+
+  @override
+  State<LeoLogin> createState() => _LeoLoginState();
+}
+
+class _LeoLoginState extends State<LeoLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
