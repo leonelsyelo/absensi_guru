a32acff6a80fc46e2f6e615a275d4d59ef2c96ea
LENOVO <leonelsyelo1@gmail.com>
2026-05-06 11:31:23 +0700

    Initial commit

---
+++ lib/leo_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LeoScreen extends StatefulWidget {
+  const LeoScreen({super.key});
+
+  @override
+  State<LeoScreen> createState() => _LeoScreenState();
+}
+
+class _LeoScreenState extends State<LeoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
