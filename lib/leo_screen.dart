59309d674d87e859271238c50fc46a1804f97afe
LENOVO <leonelsyelo1@gmail.com>
2026-05-06 13:01:28 +0700

    kembut kembut

--- lib/leo_screen.dart
+++ lib/leo_screen.dart
@@ -1,15 +1,70 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class LeoScreen extends StatefulWidget {
-  const LeoScreen({super.key});
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({super.key});
 
   @override
-  State<LeoScreen> createState() => _LeoScreenState();
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
 }
 
-class _LeoScreenState extends State<LeoScreen> {
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  late Timer t;
+
   @override
+  void initState() {
+    t = Timer.periodic(
+      const Duration(seconds: 4),
+          (timer) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      ),
+    );
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(colors: [Colors.grey, Colors.black]),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "People App",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 150),
+            Icon(Icons.people, size: 80, color: Colors.white),
+            SizedBox(height: 150),
+            Text(
+              'Developed By',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'Leo',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
